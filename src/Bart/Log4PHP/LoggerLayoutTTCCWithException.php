52e8aec0a399e0a21d6cdc42ea33977783d4ce10
Benjamin VanEvery <bvanevery@box.com>
2012-12-04 11:51:39 -0800

    LoggerLayoutTTCC is deprecated

--- src/Bart/Log4PHP/LoggerLayoutTTCCWithException.php
+++ src/Bart/Log4PHP/LoggerLayoutTTCCWithException.php
@@ -3,22 +3,9 @@
 namespace Bart\Log4PHP;
 
 /**
- * Formatting class that will log exception details when present
- * @link http://stackoverflow.com/questions/7107413/how-to-output-exception-information-in-log-file-with-log4php/
+ * @deprecated Use LoggerLayoutPatternWithException
  */
-class LoggerLayoutTTCCWithException extends \LoggerLayoutTTCC
+class LoggerLayoutTTCCWithException extends LoggerLayoutPatternWithException
 {
-	public function format(\LoggerLoggingEvent $event)
-	{
-		$format = parent::format($event);
-
-		$throwableInfo = $event->getThrowableInformation();
-		if ($throwableInfo === null) {
-			return $format;
-		}
-
-		$renderer = new \LoggerRendererException();
-		return $format . $renderer->render($throwableInfo->getThrowable());
-	}
 }
 
