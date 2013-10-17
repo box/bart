fcdaa3e6aa6205a0959608bc788eeb549c4a2d7c
Geoffrey Anderson <geoff@geoffreyanderson.net>
2013-10-16 20:40:21 -0700

    Allow for custom PHPUNIT flags

--- build/post-commit.sh
+++ build/post-commit.sh
@@ -5,6 +5,9 @@
 
 set +x
 
+# Set our default phpunit flags
+PHPUNIT_FLAGS="--log-junit build/reports/phpunit.xml $PHPUNIT_FLAGS"
+
 # Assert assumption that we're running from the project root directory
 # This works for all of my projects, but someone else may want to modify this
 if [[ ! -d "test" ]]; then
@@ -23,6 +26,6 @@ set -x
 # PHPUnit doesn't have a --no-colors option
 # For jenkins, check out https://wiki.jenkins-ci.org/display/JENKINS/AnsiColor+Plugin
 phpunit \
-  --log-junit build/reports/phpunit.xml \
+  $PHPUNIT_FLAGS \
   test/
 
