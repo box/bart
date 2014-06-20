b78dc7f12575238e5eef20e5f97555031bfc37a2
Benjamin VanEvery <bvanevery@box.com>
2014-06-20 15:04:05 -0700

    Summarize strings

--- src/Bart/Primitives/Strings.php
+++ src/Bart/Primitives/Strings.php
@@ -28,4 +28,42 @@ class Strings
 		return implode($replacement, $ucs);
 	}
 
+	/**
+	 * Ensure that text is no longer than $maxLength. If so, truncate $subject at word boundary
+	 * closest before $maxLength and replace with $suffix
+	 * @param string $subject Original text to summarize
+	 * @param int $maxLength
+	 * @param string $suffix Text to append at end if $subject is longer than $maxLength,
+	 * @return string Truncated text followed by $suffix if necessary
+	 */
+	public static function summarize($subject, $maxLength, $suffix = '...')
+	{
+		if (!$subject) {
+			return '';
+		}
+
+		if (strlen($subject) <= $maxLength) {
+			return $subject;
+		}
+
+		$suffixLen = strlen($suffix);
+		// Leave space for the suffix
+		$index = $maxLength - $suffixLen;
+		// Look for a natural break after which to append the suffix
+		while ($index > 0) {
+			if (ctype_space($subject[$index])) {
+				break;
+			}
+
+			$index -= 1;
+		}
+
+		// Maybe it's just one very long word
+		// NOTE not covering case where $maxLength <= $suffixLen
+		if ($index <= 0) {
+			return substr($subject, 0, $maxLength - $suffixLen) . $suffix;
+		}
+
+		return substr($subject, 0, $index) . $suffix;
+	}
 } 
--- test/Bart/Primitives/StringsTest.php
+++ test/Bart/Primitives/StringsTest.php
@@ -5,21 +5,65 @@ use Bart\BaseTestCase;
 
 class StringsTest extends BaseTestCase
 {
-	public function data_provider_test_titleize()
+	public function dataProviderTestTitleize()
 	{
-		return array(
-			array('hello-world', '-', '_', 'Hello_World'),
-			array('hello-world', '-', null, 'Hello-World'),
-			array('hello<<>>world', '<<>>', null, 'Hello<<>>World'),
-		);
+		return [
+			['hello-world', '-', '_', 'Hello_World'],
+			['hello-world', '-', null, 'Hello-World'],
+			['hello<<>>world', '<<>>', null, 'Hello<<>>World'],
+		];
+	}
+
+	public function dataProviderTestSummarizeWithoutSpecialSuffix()
+	{
+		$foxy = 'The quick brown fox jumps over the lazy dog';
+		$foxyLen = strlen($foxy);
+		return [
+			[$foxy, $foxyLen, $foxy],
+			[$foxy, $foxyLen + 1, $foxy],
+			// Check against another type of white space
+			[$foxy . "\n$foxy", $foxyLen + 5, $foxy . '...'],
+			[$foxy, $foxyLen - 1, 'The quick brown fox jumps over the lazy...'],
+			[$foxy, $foxyLen - 3, 'The quick brown fox jumps over the...'],
+			[$foxy, $foxyLen - 5, 'The quick brown fox jumps over the...'],
+			// A subject with no white space
+			['123456789', 5, '12...'],
+		];
+	}
+
+	public function dataProviderTestSummarizeWithSuffix()
+	{
+		$foxy = 'The quick brown fox jumps over the lazy dog';
+		$foxyLen = strlen($foxy);
+		return [
+			[$foxy, $foxyLen, '...', $foxy],
+			[$foxy, $foxyLen - 1, '*', 'The quick brown fox jumps over the lazy*'],
+			// Just one character too long
+			[$foxy . ' a', $foxyLen + 1, '*', 'The quick brown fox jumps over the lazy dog*'],
+		];
 	}
 
 	/**
-	 * @dataProvider data_provider_test_titleize
+	 * @dataProvider dataProviderTestTitleize
 	 */
-	public function test_titleize($subject, $delimiter, $replacement, $expected_title)
+	public function testTitleize($subject, $delimiter, $replacement, $expected_title)
 	{
 		$this->assertEquals($expected_title, Strings::titleize($subject, $delimiter, $replacement));
 	}
+
+	/**
+	 * @dataProvider dataProviderTestSummarizeWithoutSpecialSuffix
+	 */
+	public function testSummarizeWithoutSpecialSuffix($subject, $maxLength, $expected)
+	{
+		$this->assertEquals($expected, Strings::summarize($subject, $maxLength));
+	}
+
+	/**
+	 * @dataProvider dataProviderTestSummarizeWithSuffix
+	 */
+	public function testSummarizeWithSuffix($subject, $maxLength, $suffix, $expected)
+	{
+		$this->assertEquals($expected, Strings::summarize($subject, $maxLength, $suffix));
+	}
 }
- 
