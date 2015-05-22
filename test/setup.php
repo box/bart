ed7d8aeb96a17e3f38e7b33a0b4eced6f4733bf7
David Duke <dduke@box.com>
2015-05-22 12:59:26 -0700

    Add log4php to composer.json
    Previously we were installing log4php via Pear.  Pear cannot be easily
    installed on OSX, and most packages are moving away from Pear.
    log4php can be installed via composer though.

--- test/setup.php
+++ test/setup.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL | E_STRICT);
 // $root/test/setup.php
 $root = dirname(__DIR__) . '/';
 require_once $root . 'src/Bart/bart-common.php';
+require_once $root . 'vendor/autoload.php';
 
 // So tests can use static methods in other tests
 \Bart\Autoloader::register_autoload_path($root . 'test');
@@ -14,6 +15,5 @@ require_once $root . 'test/Bart/BaseTestCase.php';
 
 // If you don't want to use log4php, then we can create a stub class for it;
 // ...but at the time being, it's not a priority
-require_once 'log4php/Logger.php';
 \Bart\Log4PHP::initForConsole('off');
 
