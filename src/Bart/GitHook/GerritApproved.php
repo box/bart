5f6ed68301facd31fa229085c140a0eb0e624169
Benjamin VanEvery <bvanevery@box.com>
2015-08-24 22:07:51 -0700

    Use latest Gerrit API for GerritApproved
    And stop using deprecated methods!

--- src/Bart/GitHook/GerritApproved.php
+++ src/Bart/GitHook/GerritApproved.php
@@ -2,49 +2,37 @@
 namespace Bart\GitHook;
 
 use Bart\Diesel;
+use Bart\Gerrit\Change;
 use Bart\Git\Commit;
+use Bart\Git\GitException;
 
 /**
  * Enforces that a commit is approved in gerrit
  */
 class GerritApproved extends GitHookAction
 {
-	/** @var \Bart\Gerrit\Api */
-	private $api;
-
-	public function __construct()
-	{
-		parent::__construct();
-
-		/** @var \Bart\Gerrit\Api api */
-		$this->api = Diesel::create('Bart\Gerrit\Api');
-	}
-
 	/**
-	 * Fails if review is not approved in Gerrit
-	 * @param Commit $commit
-	 * @throws GitHookException
+	 * Fails if review is not approved & verified in Gerrit
+	 * @param Commit $commit A git commit with a Change-Id
+	 * @throws GitHookException If Change-Id not found or the review is not approved or verified
 	 */
 	public function run(Commit $commit)
 	{
-		// Let exception bubble up if no change id
-		$changeId = $commit->gerritChangeId();
-
-		$data = null;
-		try
-		{
-			$this->logger->debug("Getting data from gerrit: {$changeId}");
-			$data = $this->api->getApprovedChange($changeId, $commit->revision());
-		}
-		catch(\Exception $e)
-		{
-			throw new GitHookException("Error getting Gerrit review info for {$changeId}", $e->getCode(), $e);
+		try {
+			$changeId = $commit->gerritChangeId();
+		} catch (GitException $e) {
+			$this->logger->warn("{$e->getMessage()}. Skipping commit.");
+			throw new GitHookException("Couldn't get Change-Id for {$commit}", $e->getCode(), $e);
 		}
 
-		if ($data == null)
-		{
-			throw new GitHookException ('An approved review was not found in Gerrit for'
-					. " commit {$commit} with Change-Id {$changeId}");
+		/** @var \Bart\Gerrit\Change $change */
+		$change = Diesel::create('\Bart\Gerrit\Change', $changeId);
+
+		if (!$change->isReviewedAndVerified()) {
+			$msg = "Could not find an approved & verified change in Gerrit for change $changeId in commit $commit";
+			$this->logger->info($msg);
+
+			throw new GitHookException($msg);
 		}
 
 		$this->logger->info('Gerrit approved.');
