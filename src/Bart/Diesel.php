f3a54787c86c7b17d4e1b4966f8e31c5a46dc5e5
Nadeem Ahmad <nahmad@box.com>
2015-10-08 16:23:23 -0700

    Modify Diesel to support TestScribe
    This change paves the way for automatically generating unit tests in Bart-dependent projects, using https://github.com/box/TestScribe.

--- src/Bart/Diesel.php
+++ src/Bart/Diesel.php
@@ -9,6 +9,8 @@ namespace Bart;
  */
 class Diesel
 {
+	private static $isTestGeneratorRun = null;
+
 	/**
 	 * @var array Registry of all instantiation methods
 	 */
@@ -33,6 +35,11 @@ class Diesel
 		$arguments = func_get_args();
 		$className = array_shift($arguments);
 
+		$mockObj = self::checkTestScribe($className, $arguments);
+		if ($mockObj !== null) {
+			return $mockObj;
+		}
+
 		// If a method has been registered
 		if (array_key_exists($className, self::$instantiators)) {
 			$instantiator = self::$instantiators[$className];
@@ -41,6 +48,46 @@ class Diesel
 		}
 
 		return self::createInstance($className, $arguments);
+	}
+
+	/**
+	 * Return true when PHPUnit test generator is loaded.
+	 * See comments in __callStatic method for more details.
+	 *
+	 * @return bool
+	 */
+	private static function isTestGeneratorRun()
+	{
+		if (self::$isTestGeneratorRun !== null) {
+			// Cache the result of this run time check
+			// to improve performance since resolve is called many times
+			// at run time.
+			return self::$isTestGeneratorRun;
+		}
+
+		// The test generator should have loaded the class and no autoloader should be needed
+		// when the class exists.
+		self::$isTestGeneratorRun = class_exists('\Box\TestScribe\App', false);
+
+		return self::$isTestGeneratorRun;
+	}
+
+	/**
+	 * Replace real instance with the given instance typically a mock object.
+	 *
+	 * @param string $className
+	 * @param object $instance
+	 *
+	 * @return void
+	 */
+	public static function overwrite($className, $instance)
+	{
+		self::registerInstantiator(
+			$className,
+			function () use ($instance) {
+				return $instance;
+			}
+		);
 	}
 
 	/**
@@ -124,6 +171,37 @@ class Diesel
 	public static function disableDefault()
 	{
 		self::$allowDefaults = false;
+	}
+
+
+	private static function checkTestScribe($className, $arguments)
+	{
+
+		/** @noinspection PhpUndefinedNamespaceInspection */
+		/** @noinspection PhpUndefinedClassInspection */
+		if (self::isTestGeneratorRun()
+			&& \Box\TestScribe\App::$shouldInjectMockObjects
+		) {
+			// This code block is to support PHPUnit test generator.
+			// @see https://github.com/box/TestScribe
+			// The class 'Box\TestScribe\App' should only be loaded
+			// when this method is executed by the test generator.
+			// It allows the test generator to substitute the real class instance with a
+			// mock object controlled by the test generator so that the test
+			// generator can monitor the creation and execution of the class
+			// being resolved.
+			/** @noinspection PhpUndefinedNamespaceInspection */
+			$mock = \Box\TestScribe\App::createMockedInstance(
+				$className,
+				$arguments
+			);
+			if ($mock) {
+				return $mock;
+			}
+			// If null is returned by the unit test generator,
+			// it means that the developer has decided not to mock this class.
+			// Proceed with creating the real object.
+		}
 	}
 }
 
