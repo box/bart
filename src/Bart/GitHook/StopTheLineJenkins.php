a534459655df8c04324bfb548df8f21e53607c6e
Nadeem Ahmad <nadeemahmad91@gmail.com>
2016-03-28 11:35:33 -0700

    Change tabs to spaces

--- src/Bart/GitHook/StopTheLineJenkins.php
+++ src/Bart/GitHook/StopTheLineJenkins.php
@@ -13,57 +13,57 @@ use Bart\Jenkins\Job;
  */
 class StopTheLineJenkins extends GitHookAction
 {
-	/** @var Job $job **/
-	private $job;
+    /** @var Job $job * */
+    private $job;
 
-	public function __construct()
-	{
-		parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
 
-		/** @var JenkinsConfig $jenkinsConfig */
-		$jenkinsConfig = Diesel::create('\Bart\Jenkins\JenkinsConfig');
+        /** @var JenkinsConfig $jenkinsConfig */
+        $jenkinsConfig = Diesel::create('\Bart\Jenkins\JenkinsConfig');
 
-		/** @var Connection $connection */
-		$connection = Diesel::create(
-			'\Bart\Jenkins\Connection',
-			$jenkinsConfig->domain(),
-			$jenkinsConfig->protocol(),
-			$jenkinsConfig->port()
-		);
+        /** @var Connection $connection */
+        $connection = Diesel::create(
+            '\Bart\Jenkins\Connection',
+            $jenkinsConfig->domain(),
+            $jenkinsConfig->protocol(),
+            $jenkinsConfig->port()
+        );
 
-		$user = $jenkinsConfig->user();
-		$token = $jenkinsConfig->token();
-		if ($user !== null && $token !== null) {
-			$connection->setAuth($user, $token);
-		}
+        $user = $jenkinsConfig->user();
+        $token = $jenkinsConfig->token();
+        if ($user !== null && $token !== null) {
+            $connection->setAuth($user, $token);
+        }
 
-		/** @var Job job */
-		$this->job = Diesel::create('\Bart\Jenkins\Job', $connection, $jenkinsConfig->jobLocation());
-	}
+        /** @var Job job */
+        $this->job = Diesel::create('\Bart\Jenkins\Job', $connection, $jenkinsConfig->jobLocation());
+    }
 
-	/**
-	 * @param Commit $commit
-	 * @throws GitHookException
-	 * @throws GitException
+    /**
+     * @param Commit $commit
+     * @throws GitHookException
+     * @throws GitException
      */
-	public function run(Commit $commit)
-	{
-		if ($this->job->isHealthy()) {
-			$this->logger->debug('Jenkins job is healthy.');
-			return;
-		}
+    public function run(Commit $commit)
+    {
+        if ($this->job->isHealthy()) {
+            $this->logger->debug('Jenkins job is healthy.');
+            return;
+        }
 
-		$this->logger->info('Jenkins job is not healthy...asserting that commit message contains {buildfix} hash');
-		$messageSubject = $commit->messageSubject();
+        $this->logger->info('Jenkins job is not healthy...asserting that commit message contains {buildfix} hash');
+        $messageSubject = $commit->messageSubject();
 
-		$buildFixDirective = Directives::BUILD_FIX();
+        $buildFixDirective = Directives::BUILD_FIX();
 
-		// Check if commit has buildfix directive
-		if (preg_match("/{$buildFixDirective->value()}/", $messageSubject) > 0) {
-			$this->logger->info("Commit has {$buildFixDirective} directive. It attempts to fix build");
-			return;
-		}
+        // Check if commit has buildfix directive
+        if (preg_match("/{$buildFixDirective->value()}/", $messageSubject) > 0) {
+            $this->logger->info("Commit has {$buildFixDirective} directive. It attempts to fix build");
+            return;
+        }
 
-		throw new GitHookException('Jenkins not healthy and commit does not fix it');
-	}
+        throw new GitHookException('Jenkins not healthy and commit does not fix it');
+    }
 }
